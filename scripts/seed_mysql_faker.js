da1ef666f695d78c070509fc4083b927656a2b86
iHPhuc <tranlehoangphuc0309@gmail.com>
2025-03-10 20:36:21 +0700

    feat: add MySQL seeding script and update dependencies

---
+++ scripts/seed_mysql_faker.js
@@ -0,0 +1,59 @@
+
+//const { mysql } = require('../config/mysql');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const { faker } = require('@faker-js/faker');
+
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST || 'localhost',
+  user: process.env.MYSQL_USER || 'root',
+  database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD || '',
+  port: parseInt(process.env.MYSQL_PORT) || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+async function seedDatabase() {
+  const connection = await pool.getConnection();
+  console.log('Connected to MySQL');
+  
+  try {
+    // Tạo dữ liệu employee
+    console.log('Seeding employees...');
+    const employeeData = [];
+    for (let i = 0; i < 10000; i++) {
+      employeeData.push([
+        1000 + i, // Employee_Number
+        i + 1, // idEmployee
+        faker.person.lastName(),
+        faker.person.firstName(),
+        faker.number.int({ min: 100000000, max: 999999999 }), // SSN
+        faker.finance.amount(10, 50, 2), // Pay_Rate
+        faker.number.int({ min: 1, max: 5 }), // PayRates_id
+        faker.number.int({ min: 5, max: 20 }), // Vacation_Days
+        faker.number.int({ min: 0, max: 99 }), // Paid_To_Date
+        faker.number.int({ min: 0, max: 99 }), // Paid_Last_Year
+      ]);
+    }
+    await connection.query(
+      'INSERT INTO employee (Employee_Number, idEmployee, Last_Name, First_Name, SSN, Pay_Rate, PayRates_id, Vacation_Days, Paid_To_Date, Paid_Last_Year) VALUES ?;',
+      [employeeData]
+    );
+    console.log('✅ Inserted 10,000 employees');
+  } catch (error) {
+    console.error('❌ Error inserting data:', error);
+  } finally {
+    connection.release();
+    console.log('Database seeding completed.');
+  }
+}
+
+seedDatabase();
